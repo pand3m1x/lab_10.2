04cc5dc1ebbb701c2cbfccc5d48eea9ad1edaab8
TheRedQueen <pand3m1x@gmail.com>
2026-03-02 13:40:36 -0500

    ❀⋆.ೃ࿔*:・ Demo is stylized and hook is being called into DebounceDemo ❀⋆.ೃ࿔*:・

--- lab2/src/App.jsx
+++ lab2/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import UsePaginationDemo from './components/UsePaginationDemo'
+import UseDebounceDemo from './components/UseDebounceDemo'
 
 function App() {
 
   return (
-    <div style={{border:"2px solid green", borderRadius:"10px"}}>
-     <UsePaginationDemo></UsePaginationDemo>
-    </div>
+    <>
+      <div style={{border:"2px solid green", borderRadius:"10px"}}>
+      <UsePaginationDemo></UsePaginationDemo>
+      </div>
+      <UseDebounceDemo></UseDebounceDemo>
+    </>
   )
 }
 
--- lab2/src/components/UseDebounceDemo.jsx
+++ lab2/src/components/UseDebounceDemo.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import useDebounce from '../hooks/useDebounce'
 
 function DebounceDemo(){
 
+  const [ search, setSearch ] = useState("") //search bar
+  const [ searchResults, setSearchResults] = useState([]) //array of results
+  const debouncedSearch = useDebounce(search, 1000);
+
+  useEffect(() => {
+    console.log("Searching:",search);
+  },[debouncedSearch])
+
+  const handleInputChange = (e) => {setSearch(e.target.value);}
+
   return(
     <>
-      <div style={{border:"2px solid red"}}>
-        
+      <div style={{border:"2px solid red", borderRadius:"10px"}}>
+        <h2>Debounce Demo</h2>
+        <p>Delay by milli-seconds: <i>1000ms</i></p>
+        <input type="search" placeholder="Search request here..." value={search} onChange={handleInputChange}/>
+        <p>Current Input: <i>{search}</i></p>
+        <p style={{fontSize:".8rem"}}><i>Debounced Value (after 1000ms)</i></p>
+        <p>Search Result:</p>
+          <ul>
+            {searchResults.map((result,index) => (<li key={index}>{result}</li>))}
+          </ul>
       </div>
     </>
   );
